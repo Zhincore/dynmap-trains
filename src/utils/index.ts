4746ccefccfffa54ba1c0c8e416d92e0c8451233
Curitis <curitistk@gmail.com>
2024-08-05 21:07:35 +0800

    Fix error if the world does not exist in dynmap.worlds

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,6 +6,10 @@ export type Unarray<T> = T extends readonly (infer P)[] ? P : T;
 
 export function getMapForDimension(dimension: string, dynmap: DynMap, config: IConfig) {
   const worldName = Object.entries(config.worlds).find(([_, v]) => v == dimension)?.[0];
-  const world = worldName ? dynmap.worlds[worldName] : null;
-  return world.maps[dynmap.maptype.options.name] ?? world.maps[Object.keys(world.maps)[0]];
+  if (worldName && Reflect.has(dynmap.worlds, worldName)) {
+    const world = dynmap.worlds[worldName];
+    return world.maps[dynmap.maptype.options.name] ?? world.maps[Object.keys(world.maps)[0]];
+  } else {
+    return null
+  }
 }
